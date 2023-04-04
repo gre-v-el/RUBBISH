0b64966cc10df5e251830954d4aebc089dc6d8e7
gre-v-el <myszkier.gabriel@gmail.com>
2023-04-04 17:53:56 +0200

    Added frequent token pair identification and merging

--- src/main.rs
+++ src/main.rs
@@ -55,20 +55,71 @@ fn shatter_words(words: &Vec<(String, u32)>, vocab: &Vec<String>) -> Result<Vec<
 	Ok(shattered)
 }
 
+fn print_lexicon(lexicon: &Vec<(Vec<usize>, u32)>, vocab: &Vec<String>) {
+	for (entry, frequency) in lexicon {
+		print!("{:?} = |", entry);
+		for t in entry {
+			print!("{}|", vocab[*t]);
+		}
+		println!(": {frequency}");
+	}
+}
+
+fn most_frequent_pair(lexicon: &Vec<(Vec<usize>, u32)>) -> (usize, usize) {
+	let mut best_pair = (0, 0);
+	let mut largest_frequency = 0;
+
+	let mut frequencies = HashMap::new();
+
+	for (entry, frequency) in lexicon {
+		for pair in entry.windows(2) {
+			let key = (pair[0], pair[1]);
+			if let Some(f) = frequencies.get_mut(&key) {
+				*f += frequency;
+				if *f > largest_frequency {
+					best_pair= key;
+					largest_frequency = *f;
+				}
+			}
+			else {
+				frequencies.insert(key, *frequency);
+			}
+
+			frequencies.insert(key, if let Some(f) = frequencies.get(&key) {f+1} else {1});
+		}
+	}
+
+	best_pair
+}
+
+fn merge_tokens(lexicon: &mut Vec<(Vec<usize>, u32)>, pair: (usize, usize), new: usize) {
+
+	for (entry, _) in lexicon {
+		for i in 0..(entry.len()-1) {
+			if entry[i] == pair.0 && entry[i+1] == pair.1 {
+				entry.remove(i+1);
+				entry[i] = new;
+			}
+		}
+	}
+}
+
 fn main() {
 	let mut corpus = read_to_string(".\\corpora\\TOKE.txt").unwrap();
 	corpus.make_ascii_lowercase();
 
 	let words = corpus_to_words(&corpus);
-	let vocab = words_to_vocab(&words);
+	let mut vocab = words_to_vocab(&words);
 
-	let lexicon = shatter_words(&words, &vocab).unwrap();
+	let mut lexicon = shatter_words(&words, &vocab).unwrap();
 
-	for (entry, frequency) in lexicon {
-		print!("{:?} = |", entry);
-		for t in entry {
-			print!("{}|", vocab[t]);
-		}
-		println!(": {frequency}");
-	}
+
+	let pair = most_frequent_pair(&lexicon);
+	let new_token = vocab[pair.0].clone() + &vocab[pair.1];
+	vocab.push(new_token);
+	let new_token_id = vocab.len() - 1;
+	merge_tokens(&mut lexicon, pair, new_token_id);
+
+	print_lexicon(&lexicon, &vocab);
+
 }
