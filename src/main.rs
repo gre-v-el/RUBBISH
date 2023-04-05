b0da7550ebd4d51a8ac567da079dc58632be571f
gre-v-el <myszkier.gabriel@gmail.com>
2023-04-05 07:35:21 +0200

    Added progress messages, added source data to tokens

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::{File, read_to_string}, io::BufReader, collections::{HashSet, HashMap}, ops::Index};
+use std::{fs::{File, read_to_string}, io::{BufReader, stdout, Write}, collections::{HashSet, HashMap}, ops::Index, time::SystemTime};
 
 fn corpus_to_words(corpus: &str) -> Vec<(String, u32)> {
 	let mut words = HashMap::new();
@@ -13,22 +13,22 @@ fn corpus_to_words(corpus: &str) -> Vec<(String, u32)> {
 	to_sort
 }
 
-fn words_to_vocab(words: &Vec<(String, u32)>) -> Vec<String> {
+fn words_to_vocab(words: &Vec<(String, u32)>) -> Vec<(Option<(usize, usize)>, String)> {
 	let mut set = HashSet::new();
 
 	for (word, _) in words {
 		for char in word.chars() {
-			set.insert(format!("{char}"));
+			set.insert((None, format!("{char}")));
 		}
 	}
 
-	let mut v = set.into_iter().collect::<Vec<String>>();
-	v.sort_unstable();
+	let mut v = set.into_iter().collect::<Vec<(Option<(usize, usize)>, String)>>();
+	v.sort_unstable_by(|p, q| {p.1.cmp(&q.1)});
 	v
 }
 
 // todo: binary search?
-fn shatter_words(words: &Vec<(String, u32)>, vocab: &Vec<String>) -> Result<Vec<(Vec<usize>, u32)>, String> {
+fn shatter_words(words: &Vec<(String, u32)>, vocab: &Vec<(Option<(usize, usize)>, String)>) -> Result<Vec<(Vec<usize>, u32)>, String> {
 	let mut shattered = Vec::new();
 
 	for (word, number) in words {
@@ -36,7 +36,7 @@ fn shatter_words(words: &Vec<(String, u32)>, vocab: &Vec<String>) -> Result<Vec<
 		for c in word.chars() {
 			let target_token = format!("{c}");
 			let mut index = None;
-			for (i, t) in vocab.iter().enumerate() {
+			for (i, (_, t)) in vocab.iter().enumerate() {
 				if *t == target_token {
 					index = Some(i);
 					break;
@@ -55,11 +55,11 @@ fn shatter_words(words: &Vec<(String, u32)>, vocab: &Vec<String>) -> Result<Vec<
 	Ok(shattered)
 }
 
-fn print_lexicon(lexicon: &Vec<(Vec<usize>, u32)>, vocab: &Vec<String>) {
-	for (entry, frequency) in lexicon {
+fn print_lexicon(lexicon: &Vec<(Vec<usize>, u32)>, vocab: &Vec<(Option<(usize, usize)>, String)>, percentage: f32) {
+	for (entry, frequency) in &lexicon[(lexicon.len() as f32 * (1.0 - percentage)) as usize..] {
 		print!("{:?} = |", entry);
 		for t in entry {
-			print!("{}|", vocab[*t]);
+			print!("{}|", vocab[*t].1);
 		}
 		println!(": {frequency}");
 	}
@@ -108,31 +108,48 @@ fn merge_tokens(lexicon: &mut Vec<(Vec<usize>, u32)>, pair: (usize, usize), new:
 	}
 }
 
-fn print_vocab(vocab: &Vec<String>) {
+fn print_vocab(vocab: &Vec<(Option<(usize, usize)>, String)>) {
 	for t in vocab {
-		print!("'{t}'   ");
+		print!("'{}'   ", t.1);
 	}
 	println!()
 }
 
 fn main() {
-	let mut corpus = read_to_string(".\\corpora\\TOKE.txt").unwrap();
-	corpus.make_ascii_lowercase();
+	let start = SystemTime::now();
 
+	println!("[{:.2}] reading corpus...", start.elapsed().unwrap().as_secs_f32());
+	let corpus = read_to_string(".\\corpora\\PT.txt").unwrap();
+
+	println!("[{:.2}] normalizing corpus...", start.elapsed().unwrap().as_secs_f32());
+	let corpus = corpus.to_lowercase();
+
+	println!("[{:.2}] extracting word frequencies...", start.elapsed().unwrap().as_secs_f32());
 	let words = corpus_to_words(&corpus);
+
+	println!("[{:.2}] producing starting tokens...", start.elapsed().unwrap().as_secs_f32());
 	let mut vocab = words_to_vocab(&words);
 
+	println!("[{:.2}] tokenizing words...", start.elapsed().unwrap().as_secs_f32());
 	let mut lexicon = shatter_words(&words, &vocab).unwrap();
 
+	// print_vocab(&vocab);
+	// print_lexicon(&lexicon, &vocab);
 
-	for _ in 0..100 {
+	println!("[{:.2}] producing new tokens:", start.elapsed().unwrap().as_secs_f32());
+	let new_tokens = 10;
+	for i in 0..new_tokens {
 		let pair = most_frequent_pair(&lexicon);
-		let new_token = vocab[pair.0].clone() + &vocab[pair.1];
-		vocab.push(new_token);
+		let new_token = vocab[pair.0].1.clone() + &vocab[pair.1].1;
+		vocab.push((Some(pair), new_token));
 		let new_token_id = vocab.len() - 1;
 		merge_tokens(&mut lexicon, pair, new_token_id);
+		print!("\r[{}{}] {}/{new_tokens}", "X".repeat(i), " ".repeat(new_tokens - i), i+1);
+		stdout().flush().unwrap();
 	}
 
-	print_lexicon(&lexicon, &vocab);
+	println!("\n\n1/1000 of most frequent words:");
+	print_lexicon(&lexicon, &vocab, 0.001);
+	println!("\ntokens:");
 	print_vocab(&vocab);
 }
